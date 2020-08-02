68fd7fa1dcec189ef625306610f6828306c78e0e
Nate Sales <nate@nate.to>
2020-08-02 01:47:37 -0700

    Fix checkin

--- pages/api/checkin.js
+++ pages/api/checkin.js
@@ -3,37 +3,38 @@ import Person from '../../models/Person';
 import Location from '../../models/Location'
 
 export default async function handler(req, res) {
-    const { method } = req
+    const {method} = req
 
     await dbConnect()
 
     switch (method) {
         case 'POST':
-            const person = await Person.find({ "uid": req.body.uid });
+            const person = await Person.find({"uid": req.body.uid});
             if (!person) {
-                return res.status(400).json({ success: false });
+                return res.status(400).json({success: false});
             }
 
-            if (person.current_location) {
-                return res.status(400).json({ success: false, message: "You're already checked in!" });
+            if (person[0]["current_location"]) {
+                return res.status(400).json({success: false, message: "You're already checked in!"});
             }
 
-            const location = await Location.findById(resp.body.location);
+            const location = await Location.findById(req.body.location);
             if (!location) {
-                return res.status(400).json({ success: false, message: "Location not found" });
+                return res.status(400).json({success: false, message: "Location not found"});
             }
 
-            await Person.updateOne({ "uid": req.body.uid }, {
+            // TODO: Check if this actually succeed and return as success variable
+            await Person.updateOne({"uid": req.body.uid}, {
                 current_location: {
                     location: req.body.location,
                     time_in: Date.now()
                 }
             })
 
-            res.status(200).json({ success: true });
+            res.status(200).json({success: true});
             break
         default:
-            res.status(400).json({ success: false })
+            res.status(400).json({success: false})
             break
     }
 }
