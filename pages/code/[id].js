e7dd7b507695ff1a70fed18623440427f20223d4
Nate Sales <nate@nate.to>
2020-08-19 15:24:20 -0700

    Add checkin logic and comment [id].js

--- pages/code/[id].js
+++ pages/code/[id].js
@@ -6,25 +6,32 @@ const CodeHandler = () => {
     const router = useRouter()
     const {id} = router.query
 
-    const [userState, setUserState] = useState(null)
-
-
     useEffect(() => {
         fetchUser().then(data => {
             if (data === null) {
-                // redirect
+                window.location.href = "/api/login";
             }
-            setUserState(data)
-        }).then(() => {
-        })
+
+            if (data["current_location"] !== null) { // If checked in
+                if (data["current_location"]["_id"] === id) {
+                    fetch("/api/checkin",
+                        {method: "post"})
+                        .then(rsp => console.log(rsp));
+                } else { // TODO: Display this on screen
+                    console.log("You are trying to check out from a location that you aren't checked into.")
+                }
+            } else { // If not checked in
+                fetch("/api/checkin", {
+                    method: "POST",
+                    credentials: "include",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify({
+                        "location": id,
+                    })
+                }).then(resp => console.log(resp));
+            }
+        });
     }, []);
-
-    // useEffect(() => {
-    //     testApi();
-    // }, []);
-
-    // console.log(user.locations[user.locations.length - 1]);
-    console.log(userState);
 
     return (
         <div>
