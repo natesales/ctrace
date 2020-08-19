ec673c3d4da74dafb55f0593a6e243562be461ff
Nate Sales <nate@nate.to>
2020-08-18 22:11:48 -0700

    Reformat

--- pages/code/[id].js
+++ pages/code/[id].js
@@ -5,39 +5,26 @@ import {useEffect, useState} from "react";
 const CodeHandler = () => {
     const router = useRouter()
     const {id} = router.query
-    
-    const [ userState, setUserState ] = useState(null)
+
+    const [userState, setUserState] = useState(null)
 
 
     useEffect(() => {
         fetchUser().then(data => {
+            if (data === null) {
+                // redirect
+            }
             setUserState(data)
         }).then(() => {
         })
-    }, [])
-
-    async function testApi() {
-        const testcall = await fetch('/api/checkin', {
-            method: 'POST',
-            credentials: 'include',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                "uid": "knightss",
-                "location": "5f27b02471019f2f46a9c819"
-            }),
-        }).then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => {
-                console.log(error);
-            });
-    }
+    }, []);
 
     // useEffect(() => {
     //     testApi();
     // }, []);
 
     // console.log(user.locations[user.locations.length - 1]);
-    console.log(user);
+    console.log(userState);
 
     return (
         <div>
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+// TODO: Extract to .env
 const MONGO_URI = "mongodb+srv://admin:Uu7e2u3yFi2oQVGBgEUa8pTZACZ3JfE5TNeSnYnwddoyqyesZX8GZuiqtwDtFEKy79vauXFYGpRrn96P6k7ziqJjSdXC7M4DyX7Gy@ctrack-db0.ivwi9.mongodb.net/ctrace?authSource=admin&replicaSet=atlas-jf6fnb-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
 
 const connection = {} /* creating connection object*/
