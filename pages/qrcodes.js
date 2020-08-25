e849de04c9178df12386d1b87809339d968046b0
Nate Sales <nate@nate.to>
2020-08-25 14:10:58 -0700

    Center align QR page

--- pages/qrcodes.js
+++ pages/qrcodes.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import Grid from "@material-ui/core/Grid";
 
 export default function QRCodes() {
     const [images, setImages] = useState(null);
@@ -16,9 +17,17 @@ export default function QRCodes() {
                 images.map((image) => {
                     return (
                         <div key={image.name} className="pageBreak">
-                            <h1>{image.name}</h1>
-                            <img src={image.image} alt={image.name}/>
-                            <h3>{image.ref}</h3>
+                            <Grid container justify = "center">
+                                <h1>{image.name}</h1>
+                            </Grid>
+
+                            <Grid container justify = "center">
+                                <img src={image.image} alt={image.name}/>
+                            </Grid>
+
+                            <Grid container justify = "center">
+                                <h3>{image.ref}</h3>
+                            </Grid>
                         </div>
                     )
                 }) : <h1>Loading...</h1>}
