ce9884a526620ea1c9ddac1ad6ed1882fc200e7c
Nate Sales <nate@nate.to>
2020-08-25 17:26:48 -0700

    Create admin dashboard SidebarElement.js

---
+++ pages/dashboard/SidebarElement.js
@@ -0,0 +1,18 @@
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Link from "@material-ui/core/Link";
+import React from "react";
+
+export default function SidebarElement(props) {
+    return (
+        <Link href={props.href}>
+            <ListItem button onClick={props.onClickHandler}>
+                <ListItemIcon>
+                    {props.icon}
+                </ListItemIcon>
+                <ListItemText primary={props.name}/>
+            </ListItem>
+        </Link>
+    )
+}
