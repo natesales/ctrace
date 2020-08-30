6a3b7b5618626b3c994ec28486d71b6a09f0d2ae
Nate Sales <nate@nate.to>
2020-08-29 22:02:43 -0700

    Create change API route

---
+++ pages/api/change.js
@@ -0,0 +1,32 @@
+import dbConnect from "../../utils/dbConnect";
+import Person from "../../models/Person";
+import auth0 from "../../lib/auth0";
+
+export default auth0.requireAuthentication(async function handler(req, res) {
+    const {user} = await auth0.getSession(req);
+    const {method} = req;
+    await dbConnect();
+
+    switch (method) {
+        case "POST":
+            if (!(req.body.direction === "time_in" || req.body.direction === "time_out")) {
+                return res.status(400).json({success: false, message: "direction must be either \"time_in\" or \"time_out\""})
+            }
+
+            const person = await Person.findOne({"uid": user.nickname});
+            if (person == null) {
+                return res.status(400).json({success: false, message: "Person object not found"});
+            }
+
+            // Update the last location's direction key with the new time
+            person.locations[person.locations.length - 1][req.body.direction] = req.body.time;
+            // Push the update to the database
+            await person.save();
+
+            res.status(200).json({success: true, message: "Time updated to " + req.body.time});
+            break;
+        default:
+            res.status(400).json({success: false, message: "Default"});
+            break
+    }
+});
