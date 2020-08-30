b8bf8afb83d9ce14371f61144717aa9a48595818
Nate Sales <nate@nate.to>
2020-08-29 22:03:15 -0700

    Reformat location table

--- pages/dashboard/LocationTable.js
+++ pages/dashboard/LocationTable.js
@@ -6,7 +6,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import { makeStyles } from "@material-ui/core";
+import {makeStyles} from "@material-ui/core";
 
 // Just in case
 const useStyles = makeStyles(theme => ({}))
@@ -17,13 +17,13 @@ export default function LocationTable(props) {
     return (
         <React.Fragment>
             <Typography component="h2" variant="h6" color="primary" gutterBottom>Locations</Typography>
-            
-            <Table size="small" >
+
+            <Table size="small">
                 <TableHead>
                     <TableRow>
-                        <TableCell  >Name</TableCell>
-                        <TableCell  >Max occupancy</TableCell>
-                        <TableCell  >Current occupancy</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Max occupancy</TableCell>
+                        <TableCell>Current occupancy</TableCell>
                     </TableRow>
                 </TableHead>
                 {
@@ -31,16 +31,16 @@ export default function LocationTable(props) {
                         <TableBody>
                             {props.locations.map((location) => (
                                 <TableRow key={location.name}>
-                                    <TableCell  >{location.name}</TableCell>
-                                    <TableCell  >{location.max_occupancy}</TableCell>
-                                    <TableCell  >{location.current_occupancy}</TableCell>
+                                    <TableCell>{location.name}</TableCell>
+                                    <TableCell>{location.max_occupancy}</TableCell>
+                                    <TableCell>{location.current_occupancy}</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
                 }
             </Table>
 
-            {(props.locations === undefined || props.locations === null) ? <div style={{display: 'flex', justifyContent: 'center', paddingTop: '10px'}}> <CircularProgress /> </div> : null}
+            {(props.locations === undefined || props.locations === null) ? <div style={{display: 'flex', justifyContent: 'center', paddingTop: '10px'}}><CircularProgress/></div> : null}
         </React.Fragment>
     );
 }
